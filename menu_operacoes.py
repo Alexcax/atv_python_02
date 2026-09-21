323214d40b2aa3ba2f99b9501afd84bcb8d9b2f0
Alexsander <alexsandercavalcante18@gmail.com>
2026-09-21 10:25:16 -0300

    Enhance comments and readability in menu_operacoes.py
    Refactor menu_operacoes.py to improve readability and comments.

--- menu_operacoes.py
+++ menu_operacoes.py
@@ -1,15 +1,15 @@
 print("=== CALCULADORA ===")
-print("1 - Soma")
-print("2 - Subtração")
+print("1 - Soma")    
+print("2 - Subtração")                       # Exibe as operações disponíveis
 print("3 - Multiplicação")
 print("4 - Divisão")
 
-opcao = int(input("Escolha uma operação: "))
+opcao = int(input("Escolha uma operação: "))                  # Solicita ao usuário que escolha uma das operações
 
-numero1 = float(input("Digite o primeiro número: "))
+numero1 = float(input("Digite o primeiro número: "))          # Solicita os dois números que serão utilizados no cálculo
 numero2 = float(input("Digite o segundo número: "))
 
-match opcao:
+match opcao:                                    # Verifica qual operação foi escolhida pelo usuário, e realiza a operação dessa opção
     case 1:
         resultado = numero1 + numero2
         print("Resultado:", resultado)
@@ -23,11 +23,11 @@ match opcao:
         print("Resultado:", resultado)
 
     case 4:
-        if numero1 and numero2 != 0:
+        if numero2 != 0:                     # Verifica se é possível realizar a divisão por zero
             resultado = numero1 / numero2
             print("Resultado:", resultado)
         else:
             print("Não é possível dividir por zero.")
 
-    case _:
+    case _:                                              # Executado caso o usuário escolha uma opção que não existe
         print("Opção inválida.")
